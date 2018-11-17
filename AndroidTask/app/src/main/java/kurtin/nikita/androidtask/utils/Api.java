ff50d892b7b6623e14663a42ebd24612888109af
bubu <nikita.kurtin@gmail.com>
2018-11-17 14:29:29 +0200

    UPDATE apk

--- AndroidTask/app/src/main/java/kurtin/nikita/androidtask/utils/Api.java
+++ AndroidTask/app/src/main/java/kurtin/nikita/androidtask/utils/Api.java
@@ -5,16 +5,14 @@ package kurtin.nikita.androidtask.utils;
  */
 public final class Api {
 
-    public final static class Youtube{
-        public static final String API_KEY = "AIzaSyAaGqg8DvqEZTDDsW_-bJDQNMEwJqH3Brc";
-    }
-
     //Base url - for easy maintenance, in case the api moves to another domain
     private static final String baseUrl = "https://sandbox.cal-online.co.il";
 
     //Including endpoint for the given task
     public static final String taskUrl = baseUrl+"/youtube/playlists.json";
 
-
+    public final static class Youtube{
+        public static final String API_KEY = "AIzaSyAaGqg8DvqEZTDDsW_-bJDQNMEwJqH3Brc";
+    }
 
 }
